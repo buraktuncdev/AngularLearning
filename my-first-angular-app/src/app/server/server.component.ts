79f97359b39478c45d027a6ab03496dc8a3dd847
Burak Tunç <buraktuncdev@gmail.com>
2020-10-11 21:11:27 +0300

    display details

--- my-first-angular-app/src/app/server/server.component.ts
+++ my-first-angular-app/src/app/server/server.component.ts
@@ -10,34 +10,35 @@ export class ServerComponent implements OnInit{
   serverId = 10;
   serverStatus = 'offline';
 
-  serverCreationStatus = 'No server was created'
+  serverCreationStatus = 'No server was created';
 
   allowNewServer = false;
 
   serverName = '';
+
   getServerStatus(){
     this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
-    return this.serverStatus
+    return this.serverStatus;
   }
 
   getColor() {
-    return this.serverStatus === 'online' ? 'green': 'red'
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 
   constructor() {
 
-    setTimeout(()=> {
+    setTimeout(() => {
       this.allowNewServer = true;
-    },2000)
+    }, 2000);
   }
 
   onCreateServer(){
-    this.serverCreationStatus = 'Server was created with name: ' + this.serverName
+    this.serverCreationStatus = 'Server was created with name: ' + this.serverName;
   }
 
   onUpdateServerName($event: Event){
     console.log(event);
-    this.serverName = (<HTMLInputElement>event.target).value;
+    this.serverName = (event.target as HTMLInputElement).value;
   }
 
   ngOnInit(): void {
